6310b7800eedfa7f2175bf16fa09f6e52b57c51a
rodrigow1985 <rodrigo152000@yahoo.com.ar>
2017-08-15 20:42:57 -0300

    Rama dedicada a utilizar el framework de diseño Bootstrap

--- src/components/layout/App/index.jsx
+++ src/components/layout/App/index.jsx
@@ -17,9 +17,16 @@ class App extends Component {
 	render() {
 
     	return(
-            <div>
-              	<h1>Webpack + React Básico</h1>
-						</div>
+				<div className="container">
+				  <h2>Simple Collapsible</h2>
+				  <p>Click on the button to toggle between showing and hiding content.</p>
+				  <button type="button" className="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button>
+				  <div id="demo" className="collapse in">
+				    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+				    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				  </div>
+				</div>
         );
     }
 }
